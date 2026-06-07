9ff12ae83843a0c7d71cc395e5e05b5fe03f145c
harshithan1706NH <harshitha.n.1706@gmail.com>
2026-06-07 11:15:45 +0530

    MongoDB Atlas connection working

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,10 @@
+console.log("db.js loaded");
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  console.log("Attempting MongoDB connection...");
+
   try {
     await mongoose.connect(
       "mongodb+srv://harshithan1706_db_user:PBewDqwBHTeWGxM9@cluster0.vrrnmi9.mongodb.net/ai_campus?retryWrites=true&w=majority"
@@ -8,8 +12,9 @@ const connectDB = async () => {
 
     console.log("MongoDB Atlas connected successfully");
   } catch (error) {
-    console.log("MongoDB connection failed:", error);
-  }
+  console.log("MongoDB connection failed:");
+  console.log(error);
+}
 };
 
 module.exports = connectDB;
