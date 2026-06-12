f572ef0ef6d6aa6b0d4c5a1dd064c27ee6737e25
harshithan1706NH <harshitha.n.1706@gmail.com>
2026-06-12 20:20:26 +0530

    Added MONGO URI

--- backend/db.js
+++ backend/db.js
@@ -6,8 +6,8 @@ const connectDB = async () => {
   console.log("Attempting MongoDB connection...");
 
   try {
-    await mongoose.connect(
-      "mongodb+srv://harshithan1706_db_user:PBewDqwBHTeWGxM9@cluster0.vrrnmi9.mongodb.net/ai_campus?retryWrites=true&w=majority"
+    await mongoose.connect(process.env.MONGO_URI
+      
     );
 
     console.log("MongoDB Atlas connected successfully");
